f1fb548c699f3bafb6394fe0abcf2e0ee880525b
mikearaya <mikaelaraya12@gmail.com>
2019-10-29 19:47:46 +0300

    added modifiers and applied constraints

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -5,42 +5,43 @@ contract Event is ERC721Full {
     
     string public name;
     uint256 public startDate;
+    uint256 private ticketId;
     uint256 public endDate;
     uint256 public available;
+    uint8 private MAX_PURCHASE = 5;
     string location;
+    bool canceled;
     uint ticketPrice;
     address payable public  owner;
-    bool canceled;
-    bool public allTransfer;
 
     constructor(address payable _organizer, string memory _name, uint _start, uint _end,  uint supply, uint _ticketPrice) ERC721Full(_name, "TKT") public {
+
         name = _name;
         startDate = _start;
         endDate = _end;
         ticketPrice = _ticketPrice;
         available = supply;
         owner = _organizer;
+   
     }
 
 
     function purchaseTicket(uint quantity) public payable {
         require(msg.value >= ticketPrice.mul(quantity), "not enough money sent");
-        _mint(msg.sender,now);
-        owner.transfer(msg.value);      
+        require(quantity <= MAX_PURCHASE, "can not purchase more than 5 ticket at once");
+        for(uint8 i = 0; i < quantity; i++) {
+            ticketId++;
+            _mint(msg.sender,ticketId);    
+        }
     }
-
+    
     function transferTicket(address _to, uint _tokenId) public {
-        require(allowTransfer);
-        require(address(0) != _to);
-        transferFrom(msg.sender, _to, _tokenId);
-    }
-
-    function getOwnersTicket(address _owner) public view returns(uint[] memory) {
-        return _tokensOfOwner(_owner);
+    require(address(0) != _to, "invalid address provided");
+    transferFrom(msg.sender, _to, _tokenId);
     }
 
 
-    function isTicketValid(address _owner, uint _tokenId)public returns(bool) {
+    function isTicketValid(address _owner, uint _tokenId) onlyOwner public returns(bool) {
         if(ownerOf(_tokenId) == _owner) {
             _burn(_tokenId);
             return true;
@@ -48,30 +49,37 @@ contract Event is ERC721Full {
             return false;
         }
     }
-
-    function allowTransfer() public {
-        allowTransfer = !allowTransfer;
-    }
-
-    function cancelEvent() public {
-      require(now > startDate);
+  
+  function cancelEvent() onlyOwner public {
+      require(now > startDate, "can not cancel event after it has started");
       canceled = true;
+  }
+  
+    function getOwnersTicket(address _owner) public view returns(uint[] memory) {
+        return _tokensOfOwner(_owner);
     }
-
-    function collectPayment() public {
-        require(msg.sender == owner);
-        require(now > endDate && !canceled);
+    
+    function collectPayment() onlyOwner public {
+        require(now > endDate && !canceled, "can not collect payment before the event is over");
         owner.transfer(address(this).balance);
     }
     
     function getRefund() public {
-        require(address(0) != msg.sender);
+        require(address(0) != msg.sender, "invalid address provided");
+        require(canceled, "event is only available for cacanceled events");
         uint[] memory tokens = _tokensOfOwner(msg.sender);
+        
+        require(tokens.length > 0, "no tokens found under the given user");
         for(uint8 i = 0; i < tokens.length; i++) {
             _burn(tokens[i]);    
         }
         msg.sender.transfer(ticketPrice.mul(tokens.length));
         
+    }
+    
+      modifier onlyOwner {
+        require(msg.sender == owner, "only event owner has the right to collect payment");
+        _;
     }
 
 }
