a4708815b735fdf601e24697a366a13fa62a24cf
mikearaya <mikaelaraya12@gmail.com>
2019-11-14 19:25:34 +0300

    integrated web3 with angular

---
+++ frontend/migrations/2_contracts_migration.js
@@ -0,0 +1,8 @@
+const Migrations = artifacts.require("Migrations");
+const EventFactory  = artifacts.require("EventFactory");
+
+
+module.exports = function(deployer) {
+  deployer.deploy(Migrations, {overwrite: false});
+  deployer.deploy(EventFactory, {overwrite: false});
+};
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+declare var require: any;
+import { Component, HostListener, NgZone } from "@angular/core";
+const Web3 = require("web3");
+const contract = require("truffle-contract");
+const coinArtifacts = require("../../build/contracts/Migrations.json");
+declare var window: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'frontend';
+  Coin = contract(coinArtifacts);
+  coinInstance: any;
+
+  account: any;
+  accounts: any;
+  web3: any;
+
+  constructor(private _ngZone: NgZone) {}
+
+  @HostListener("window:load")
+  windowLoaded() {
+    this.checkAndInstantiateWeb3();
+    this.onReady();
+  }
+
+  checkAndInstantiateWeb3 = () => {
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof window.web3 !== "undefined") {
+      console.warn(
+        "Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask"
+      );
+      // Use Mist/MetaMask's provider
+      this.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      console.warn(
+        "No web3 detected, falling back to Infura Ropsten"
+        //'No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it\'s inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
+      );
+      // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+      this.web3 = new Web3(
+        new Web3.providers.HttpProvider("http://localhost:8545")
+      );
+    }
+  };
+
+  onReady = () => {
+    // Bootstrap the MetaCoin abstraction for Use.
+    this.Coin.setProvider(this.web3.currentProvider);
+
+    // Get the initial account balance so it can be displayed.
+    this.web3.eth.getAccounts((err, accs) => {
+      if (err != null) {
+        alert("There was an error fetching your accounts.");
+        return;
+      }
+
+      if (accs.length === 0) {
+        alert(
+          "You are not connected to an Ethereum client. You can still browse the data, but you will not be able to perform transactions."
+        );
+        return;
+      }
+      this.accounts = accs;
+      this.account = this.accounts[0];
+
+      // This is run from window:load and ZoneJS is not aware of it we
+      // need to use _ngZone.run() so that the UI updates on promise resolution
+      this._ngZone.run(() => {
+        //Initial loading of UI
+        //Load balances or whatever
+      });
+    });
+  };
 }
