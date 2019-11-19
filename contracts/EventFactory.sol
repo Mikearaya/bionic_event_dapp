2f1f0910bab45b83dbf538c2cd4e3aaa3811b875
mikearaya <mikaelaraya12@gmail.com>
2019-11-19 19:10:07 +0300

    added comment for event factory

--- contracts/EventFactory.sol
+++ contracts/EventFactory.sol
@@ -2,8 +2,23 @@ pragma solidity ^0.5.0;
 
 import './Event.sol';
 
+/**
+@title Event factory */
 contract EventFactory {
 
+
+        /**
+        @title Event Created
+        @dev created and deployed new event and saves it in the event list array
+        @param _name title of the event to be created 
+        @param start start date of event in unix timestamp
+        @param end end date of event in unix timestamp 
+        @param supply total supply of event ticket available for the event
+        @param ticketPrice price of single ticket price in wei
+        @param description description of the event 
+        @param location location of the event
+        @return emits eveneCreated log
+        */
     Event[] public deployedEvents;
     event eventCreated(Event _address, string _name, uint _ticketPrice, string location, uint startDate, uint endDate, string indexed _filterName, string indexed _filterLocation);
     
@@ -15,6 +30,10 @@ contract EventFactory {
 
     }
 
+    /**
+    @title get deployed events
+    @dev returns list of event addresses
+    @return deployedEvents array of event address */
    function getDeployedEvents() public view returns(Event[] memory) {
         return deployedEvents;
     }
